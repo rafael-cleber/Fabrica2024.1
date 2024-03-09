3cb24ba846571e8814d60f24c5e6749301ebfc9e
Rafael Cleber <rafaelcleber@hotmail.com>
2024-03-09 20:07:04 -0300

    first commit

---
+++ api_rest/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import User
+from .serializers import UserSerializer
+
+import json
+
+@api_view(['GET'])
+def get_users(request):
+
+    if request.method == 'GET':
+        users = User.objects.all()
+
+        serializer = UserSerializer(users, many = True)
+        return Response(serializer.data)
+    
+    return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
